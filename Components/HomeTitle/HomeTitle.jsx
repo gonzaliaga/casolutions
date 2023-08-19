054cdcc890f1befeddf3a5dc2dd5271c043309ef
gonzaliaga <gonzaliaga@gmail.com>
2023-08-19 01:06:17 -0400

    ultimo por ver tiene detalle

--- Components/HomeTitle/HomeTitle.jsx
+++ Components/HomeTitle/HomeTitle.jsx
@@ -4,7 +4,7 @@ import "@/components/css/demo1.css"
 export default function HomeTitle() {
 
   return (
-    <div style={{ zIndex: 20, position: 'absolute', display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', width: '100%', height: '100%' }}>
+    <div style={{ zIndex: 1, position: 'absolute', display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', width: '100%', height: '100%' }}>
       <h2 className="content__title">7dev</h2>
       <h3 className="content__subtitle">Desarrollo Aplicaciones Web</h3>
       <h3 className="content__subsubtitle">(Click the Ball & magic appears)</h3>
